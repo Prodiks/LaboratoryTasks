a3ee55f2093ea68844073a58bc3504a6c16e707f
Pryaminin <ghzvbyby@gmail.com>
2023-05-24 19:23:28 +0300

    ЛР 8 и 9

---
+++ src/main/java/LR9/Examples/Example12.java
@@ -0,0 +1,19 @@
+package LR9.Examples;
+
+public class Example12 {
+    public static class Main {
+        public static void m(String str, double chislo) {
+            if (str == null) {
+                throw new IllegalArgumentException("Строка введена неверно");
+            }
+            if (chislo > 0.001) {
+                throw new IllegalArgumentException("Неверное число");
+            }
+        }
+
+        public static void main(String[] args) {
+            m(null, 0.000001);
+        }
+    }
+
+}
